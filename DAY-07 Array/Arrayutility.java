482ab384d144923b959ec769e819b036a6f799ad
praveen-2802 <praveenkum8899@gmail.com>
2026-07-25 17:21:35 +0530

    Day:7 Arrays

--- DAY-07 Array/Arrayutility.java
+++ DAY-07 Array/Arrayutility.java
@@ -7,19 +7,19 @@ public class Arrayutility
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of the array:");
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        int[] arr1 = new int[n];
         System.out.print("Enter the elements of the array:");
         for(int i=0;i<n;i++)
         {
-            arr[i] = sc.nextInt();
+            arr1[i] = sc.nextInt();
         }
-        return arr;
+        return arr1;
     }
-    public  static void displayarray(int[] arr)
+    public  static void displayarray(int[] arr1)
     {
-        for(int i=0;i<arr.length;i++)
+        for(int i=0;i<arr1.length;i++)
         {
-            System.out.print(arr[i]+" ");
+            System.out.print(arr1[i]+" ");
         }
     }
 }
